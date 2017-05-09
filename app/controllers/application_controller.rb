1d5cdb54088cf027bc08e816e4cf84f4353c3595
Luis Eguia <jlewicho@gmail.com>
2017-05-09 00:08:37 -0500

    Correccion de errores y más datos en resultados de busqueda de curriculums

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:name,:lastname,:photo,:password,:password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username,:name,:lastname,:photo,:password,:password_confirmation])
   end
 end
